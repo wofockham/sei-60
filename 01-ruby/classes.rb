c0ed24082db4efdc08524088595edf1709f0dc39
wofockham <certainthings@gmail.com>
2023-01-13 12:14:22 +1100

    ruby inheritance

--- 01-ruby/classes.rb
+++ 01-ruby/classes.rb
@@ -7,7 +7,23 @@
 # A strict hash: you have to predefine the shape.
 # Also, unlike a Ruby hash, an object can have custom methods (functions inside the object)
 
-class MarxBrother
+class Actor
+    def award_speech
+        "I would like to thank my agent and my partner. We did it baby!"
+    end
+
+    def deny_allegations
+        "I deny that and I don't remember and I was drunk and he's not my type" # Kevin Spacey defense
+    end
+end
+
+class Stooge < Actor
+    def terrible?
+        true
+    end
+end
+
+class MarxBrother < Actor
     attr_accessor :name, :instrument, :vice # macro to write getter and setters
 
     def initialize(n='', i='', v='lampooning authority')
