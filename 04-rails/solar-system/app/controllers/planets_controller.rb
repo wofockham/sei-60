167566214885a7647b3f08b7c974ee3ae966988e
wofockham <certainthings@gmail.com>
2023-01-17 14:47:39 +1100

    planets edit update and delete

--- 04-rails/solar-system/app/controllers/planets_controller.rb
+++ 04-rails/solar-system/app/controllers/planets_controller.rb
@@ -14,4 +14,20 @@ class PlanetsController < ApplicationController
         planet = Planet.create :name => params[:name], :image => params[:image], :orbit => params[:orbit], :diameter => params[:diameter], :mass => params[:mass], :moons => params[:moons]
         redirect_to planet_path(planet.id) # GET to show
     end
+
+    def edit
+        @planet = Planet.find params[:id]
+    end
+
+    def update
+        planet = Planet.find params[:id]
+        planet.update :name => params[:name], :image => params[:image], :orbit => params[:orbit], :diameter => params[:diameter], :mass => params[:mass], :moons => params[:moons]
+        redirect_to planet_path(planet.id) # GET to show
+    end
+
+    def destroy
+        planet = Planet.find params[:id]
+        planet.destroy
+        redirect_to planets_path # GET to index
+    end
 end
--- 04-rails/solar-system/config/routes.rb
+++ 04-rails/solar-system/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # This file will cause the most suffering today.
+  root :to => 'planets#index'
   get '/planets' => 'planets#index'
   get '/planets/new' => 'planets#new', :as => 'new_planet'
   post '/planets' => 'planets#create'
   get '/planets/:id' => 'planets#show', :as => 'planet'
+  get '/planets/:id/edit' => 'planets#edit', :as => 'edit_planet'
+  post '/planets/:id' => 'planets#update'
+  delete '/planets/:id' => 'planets#destroy'
 end
