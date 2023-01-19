86e0145de1515d4ae99e166e979f3103df4c1efe
wofockham <certainthings@gmail.com>
2023-01-19 15:39:19 +1100

    sign up and sign in

--- 04-rails/tunr/app/controllers/users_controller.rb
+++ 04-rails/tunr/app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      session[:user_id] = @user.id # log in the newly created user
       redirect_to root_path # TODO: send the user to a better page
     else
       render :new
--- 04-rails/tunr/app/models/user.rb
+++ 04-rails/tunr/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :mixtapes
     has_secure_password
+    validates :email, :uniqueness => true, :presence => true
 end
