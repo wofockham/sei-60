8d7df2d206118d8058cb29710b4db6b0194a51ee
wofockham <certainthings@gmail.com>
2023-01-31 12:34:52 +1100

    google book search via jQuery

---
+++ 06-ajax/books-jquery/js/books.js
@@ -0,0 +1,8 @@
+$('#search').on('submit', function (event) {
+    event.preventDefault();
+    const title = $('#title').val();
+    $.ajax('https://www.googleapis.com/books/v1/volumes?q=title:' + title).done(function (data) {
+        const cover = data.items[0].volumeInfo.imageLinks.thumbnail;
+        $('#cover').attr('src', cover);
+    });
+});
