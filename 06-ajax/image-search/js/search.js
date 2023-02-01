f058fe4b29d06f8ca2e5c4730f6d86c97ae6fa93
wofockham <certainthings@gmail.com>
2023-02-01 15:47:17 +1100

    image search -- images are appearing

---
+++ 06-ajax/image-search/js/search.js
@@ -0,0 +1,44 @@
+const searchFlickr = function (keywords) {
+    console.log('Searching for', keywords);
+
+    const flickrURL = 'https://api.flickr.com/services/rest';
+
+    $.getJSON(flickrURL, {
+        method: 'flickr.photos.search',
+        api_key: '2f5ac274ecfac5a455f38745704ad084',
+        text: keywords,
+        format: 'json',
+        nojsoncallback: 1 // don't bother looking this up: JSONP
+    }).done(showImages);
+};
+
+const showImages = function (results) {
+    const urls = _(results.photos.photo).map(generateURL);
+    _(urls).each(function (url) {
+        const $img = $('<img>').attr('src', url);
+        $img.appendTo('#images');
+    });
+};
+
+const generateURL = function (p) {
+    return [
+        'http://farm',
+        p.farm,
+        '.static.flickr.com/',
+        p.server,
+        '/',
+        p.id,
+        '_',
+        p.secret,
+        '_q.jpg' // change 'q' to something else for different sizes (see documentation)
+    ].join('');
+};
+
+$(document).ready(function () {
+    $('#search').on('submit', function (event) {
+        event.preventDefault();
+
+        const searchTerms = $('#query').val();
+        searchFlickr(searchTerms);
+    });
+});
