64f5868d88ce1d29356e00356b02da3df02e00e0
wofockham <certainthings@gmail.com>
2023-02-03 15:26:50 +1100

    <SocialNetwork />

---
+++ 09-react/social-network/src/components/App.js
@@ -0,0 +1,12 @@
+import Profile from './Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile name="Groucho Marx" age="45" motto="No gods no masters" pic="http://www.placekitten.com/200/200" />
+      <Profile name="Chico Marx" age="44" motto="That's a'nice" pic="http://www.placekitten.com/300/300" />
+    </div>
+  );
+}
+
+export default App;
