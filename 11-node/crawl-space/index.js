e6c59d5c253a0c6b0abf2f19cbf7bed6ff7e7870
wofockham <certainthings@gmail.com>
2023-02-23 14:49:45 +1100

    crawl space that skips awful sites

--- 11-node/crawl-space/index.js
+++ 11-node/crawl-space/index.js
@@ -1,23 +1,42 @@
 const Crawler = require('crawler');
 
+// Predicate
+const isCrawlable = (url) => {
+    const terribleSites = ['archive.today', 'ghostarchive.org', 'web.archive.org', 'reactjs.org', 'microsoft.com', 'twitter.com'];
+
+    // magic:
+    const terribleSiteDetected = terribleSites.some((awfulSite) => {
+        return url.includes(awfulSite);
+    });
+    return !terribleSiteDetected;
+
+    // 1 liner: heavy magic:
+    // return ! terribleSites.some( awfulSite => url.includes(awfulSite) );
+};
+
 const c = new Crawler({
    rateLimit: 1000,
    userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:108.0) Gecko/20100101 Firefox/108.0", 
    callback: (err, res, done) => {
-  
-        console.log(res.request.uri.href);
-        const title = res.$('title').text();
-        console.log(title);
-        console.log(); // blank line for readability
 
-        const $links = res.$('a[href^=http]'); // external links only, please
-        $links.each(function () {
-            const href = res.$(this).attr('href');
-            c.queue(href);
-        });
+        if (typeof res.$ === 'function') {
+            console.log(res.request.uri.href);
+            const title = res.$('title').text();
+            console.log(title);
+            console.log(); // blank line for readability
+    
+            const $links = res.$('a[href^=http]'); // external links only, please
+            $links.each(function () {
+                const href = res.$(this).attr('href');
+                if (isCrawlable(href)) {
+                    c.queue(href);
+                }
+            });
+        }
 
         done(); // this needs to be called everytime callback runs
    }
 });
 
-c.queue('https://lobste.rs/');
+// process.argv contains command line arguments (and some other crap)
+c.queue(process.argv[2] || 'https://lobste.rs/');
