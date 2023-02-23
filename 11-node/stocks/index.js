e0bcdff0d9fd053be389821d5ccecefdf07dfba9
wofockham <certainthings@gmail.com>
2023-02-23 11:16:08 +1100

    stocks in express

---
+++ 11-node/stocks/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const ejs = require('ejs');
+const quote = require('stock-quote');
+
+const server = express();
+server.set('view-engine', ejs);
+
+const PORT = 8888;
+
+server.get('/', (req, res) => {
+    res.render('home.ejs');
+});
+
+server.get('/info', async (req, res) => {
+    const { currentPrice } = await quote.getQuote(req.query.symbol);
+    res.render('info.ejs', {symbol: req.query.symbol, price: currentPrice});
+});
+
+server.listen(PORT, () => console.log(`Server running at http://localhost:${ PORT }/`));
+
