86eb0813c0f192adb35f4528521f13199e5a4a5a
wofockham <certainthings@gmail.com>
2023-02-27 15:46:58 +1100

    Vue fundamentals

---
+++ 10-advanced/vue-fundamentals/src/components/NumberFacts.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>Numberwang</h2>
+        <button @click="fetchFact">Fetch fact</button>
+        <button @click="clearFacts">Clear facts</button>
+        <p v-for="(fact, i) in facts" :key="i">{{ fact }}</p>
+    </div>    
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'NumberFacts',
+    data: function () {
+        return {
+            facts: []
+        };
+    },
+    methods: {
+        fetchFact: async function () {
+            const { data } = await axios.get('http://numbersapi.com/random/trivia');
+            this.facts.push(data);
+        },
+        clearFacts: function () {
+            this.facts = [];
+        }
+    }
+}
+</script>
---
+++ 10-advanced/vue-fundamentals/src/views/ExperimentsView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>Experiments</h1>
+        <ClickrButton />
+        <button>Don't click this unrelated button</button>
+
+        <hr>
+
+        <SocialProfile name="Groucho" bio="No gods no masters" pic="http://www.placekitten.com/400/600" />
+        <SocialProfile name="Harpo" bio="No gods no masters" pic="http://www.placekitten.com/400/601" />
+        <SocialProfile name="Chico" bio="No gods no masters" pic="http://www.placekitten.com/401/601" />
+
+        <hr>
+
+        <FancyCalculator />
+
+        <hr>
+
+        <NumberFacts />
+
+    </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import ClickrButton from '@/components/ClickrButton.vue'; // @ is a shortcut for `src/`
+import SocialProfile from '@/components/SocialProfile.vue';
+import FancyCalculator from '@/components/FancyCalculator.vue';
+import NumberFacts from '@/components/NumberFacts.vue';
+
+export default {
+    // Registering your components:
+    components: {
+        ClickrButton: ClickrButton,
+        SocialProfile: SocialProfile,
+        FancyCalculator: FancyCalculator,
+        NumberFacts: NumberFacts
+    }
+}
+
+</script>
